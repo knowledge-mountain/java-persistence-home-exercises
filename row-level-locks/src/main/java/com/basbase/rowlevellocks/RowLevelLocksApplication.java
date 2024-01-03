89e0c33ea29718fe6840b0b5c2eb0e4ca3af5256
luxoft_vbasanets <viktor.basanets@tesco.com>
2024-01-03 17:21:50 +0200

    add new homework

---
+++ row-level-locks/src/main/java/com/basbase/rowlevellocks/RowLevelLocksApplication.java
@@ -0,0 +1,13 @@
+package com.basbase.rowlevellocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowLevelLocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowLevelLocksApplication.class, args);
+	}
+
+}
---
+++ row-level-locks/src/test/java/com/basbase/rowlevellocks/RowLevelLocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basbase.rowlevellocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowLevelLocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
