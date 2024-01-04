33b7bbb511a46bd3d0659ab578e0717e66306c2d
luxoft_vbasanets <viktor.basanets@tesco.com>
2024-01-04 07:10:33 +0200

    init commit row-level-locks

--- row-level-locks/src/main/java/com/basbase/rowlevellocks/RowLevelLocksApplication.java
+++ row-level-locks/src/main/java/com/basbase/rowlevellocks/RowLevelLocksApplication.java
@@ -3,6 +3,8 @@ package com.basbase.rowlevellocks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigInteger;
+
 @SpringBootApplication
 public class RowLevelLocksApplication {
 
